de2cb843a7104be4fc9b2005cad2ca1ebb54635f
pani79 <emilio.panizza@gmail.com>
2019-07-19 16:36:35 -0300

    configuraciones iniciales (Bootstrap4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './componentes/interfaz/menus/menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/interfaz/menus/menu-principal/menu-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pani-c-int-menu-menuprincipal',
+  templateUrl: './menu-principal.component.html',
+  styleUrls: ['./menu-principal.component.css']
+})
+export class MenuPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
