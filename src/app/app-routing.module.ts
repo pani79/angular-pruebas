25ee066fbb0d036ff3be0d7a55bf1ec47ce565ba
pani79 <emilio.panizza@gmail.com>
2019-07-23 19:57:14 -0300

    dia 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SobreElProyectoComponent } from './componentes/paginas/sobre-el-proyecto/sobre-el-proyecto.component';
+import { HomeComponent } from './componentes/paginas/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'sobreElProyecto', component: SobreElProyectoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuPrincipalComponent } from './componentes/interfaz/menus/menu-principal/menu-principal.component';
+import { HomeComponent } from './componentes/paginas/home/home.component';
+import { SobreElProyectoComponent } from './componentes/paginas/sobre-el-proyecto/sobre-el-proyecto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuPrincipalComponent
+    MenuPrincipalComponent,
+    HomeComponent,
+    SobreElProyectoComponent
   ],
   imports: [
     BrowserModule,
